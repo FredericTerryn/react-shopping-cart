02c287920f166e6a20caa44f3bbb5c76e805d93e
Fréderic Terryn <Frederic.Terryn@esc.be>
2021-01-24 18:12:36 +0100

    items in cart count

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Cart from "./components/cart";
 import Filter from "./components/filter";
 import Products from "./components/Products";
 import data from "./data.json"
@@ -8,13 +9,31 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      cartItems: [],
       products: data.products,
       size: "",
       sort: ""
     }
   }
 
-  sortProducts = (event) =>{
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems.slice();
+    let alreadyInCart = false;
+
+    cartItems.forEach(
+      item => {
+        if (item._id === product._id) {
+          item.count++;
+          alreadyInCart = true;
+        }
+      });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 })
+    }
+    this.setState({cartItems});
+  }
+
+  sortProducts = (event) => {
     const sort = event.target.value;
     this.setState((state) => ({
       sort: sort,
@@ -32,45 +51,50 @@ class App extends React.Component {
               : -1
       )
     }));
-}
-
-filterProducts = (event) => {
-  if (event.target.value === "") {
-    this.setState({ size: event.target.value, product: data.products })
-  } else {
-    this.setState({
-      size: event.target.value,
-      products: data.products.filter(
-        product => product.availableSizes.indexOf(event.target.value) >= 0)
-    });
   }
-}
 
-render() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React Shopping Cart</a>
-      </header>
-      <main>
-        <div className="content">
-          <div className="main">
-            <Filter count={this.state.products.length}
-              size={this.state.size}
-              sort={this.state.sort}
-              filterProducts={this.filterProducts}
-              sortProducts={this.sortProducts}></Filter>
-            <Products products={this.state.products}></Products>
+  filterProducts = (event) => {
+    if (event.target.value === "") {
+      this.setState({ size: event.target.value, product: data.products })
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          product => product.availableSizes.indexOf(event.target.value) >= 0)
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">React Shopping Cart</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">
+              <Filter count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}></Filter>
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}>
+              </Products>
+            </div>
+            <div className="sidebar">
+              <Cart cartItems={this.state.cartItems}></Cart>
+            </div>
           </div>
-          <div className="sidebar">Cart items</div>
-        </div>
-      </main>
-      <footer>
-        All rights reserved. Test.Test2
+        </main>
+        <footer>
+          All rights reserved. Test.Test2
       </footer>
-    </div>
-  );
-}
+      </div>
+    );
+  }
 }
 
 export default App;
